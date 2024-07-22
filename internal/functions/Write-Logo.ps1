d4b519b862acfc72e110b97ffc2be9e2145d4b4e
MArco Kleinert <marco.kleinert@gmail.com>
2024-07-22 12:01:04 +0200

    Add Write-Logo function to display a logo and header

---
+++ internal/functions/Write-Logo.ps1
@@ -0,0 +1,27 @@
+function Write-Logo {
+
+    # Logo
+    $Logo = @"
+██╗     ███████╗████████╗██╗  ██╗ █████╗ ██╗      ███████╗ ██████╗ ██████╗ ███████╗███╗   ██╗███████╗██╗ ██████╗███████╗
+██║     ██╔════╝╚══██╔══╝██║  ██║██╔══██╗██║      ██╔════╝██╔═══██╗██╔══██╗██╔════╝████╗  ██║██╔════╝██║██╔════╝██╔════╝
+██║     █████╗     ██║   ███████║███████║██║█████╗█████╗  ██║   ██║██████╔╝█████╗  ██╔██╗ ██║███████╗██║██║     ███████╗
+██║     ██╔══╝     ██║   ██╔══██║██╔══██║██║╚════╝██╔══╝  ██║   ██║██╔══██╗██╔══╝  ██║╚██╗██║╚════██║██║██║     ╚════██║
+███████╗███████╗   ██║   ██║  ██║██║  ██║███████╗ ██║     ╚██████╔╝██║  ██║███████╗██║ ╚████║███████║██║╚██████╗███████║
+╚══════╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝ ╚═╝      ╚═════╝ ╚═╝  ╚═╝╚══════╝╚═╝  ╚═══╝╚══════╝╚═╝ ╚═════╝╚══════╝
+"@
+
+ 
+    Write-Output ""
+    Write-Output $Logo
+    Write-Output ""
+
+    # Header
+    Write-Output "Automated Processing of Microsoft Entra ID Sign-In Logs for DFIR"
+    Write-Output "(c) 2024 Martin Willing at Lethal-Forensics (https://lethal-forensics.com/)"
+    Write-Output ""
+
+    # Analysis date (ISO 8601)
+    $script:AnalysisDate = [datetime]::Now.ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ")
+    Write-Output "Analysis date: $AnalysisDate UTC"
+    Write-Output ""
+}
